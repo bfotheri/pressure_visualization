6d454707dc050e154e8ba8b40ef3f74a2724c8c2
Brett <fotheringhambrett@gmail.com>
2019-03-21 11:59:59 -0600

    Updated so it should read in pressure values

--- Sensor.py
+++ Sensor.py
@@ -9,9 +9,9 @@ import numpy as np
 print('Pressure Sensor Module Imported')
 class PressureSensor:
     def __init__(self):
-        # i2c = busio.I2C(board.SCL, board.SDA)
-        # ads = ADS.ADS1115(i2c)
-        # self.chan = AnalogIn(ads, ADS.P0)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        ads = ADS.ADS1115(i2c)
+        self.chan = AnalogIn(ads, ADS.P0)
         #For the tuple, (x,y), x is the voltage at the pressure and y is the pressure
         self.high_point = (0,0) #TODO: Read these values in from a .txt file so we don't loose the calibration each time
         self.low_point = (0,0)
@@ -19,8 +19,8 @@ class PressureSensor:
         self.y_intercept = 0
 
     def set_high_point(self,pressure):
-        # voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
         voltage = np.random.randint(300)
+        # voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
         self.high_point = (voltage,pressure)
         self.calibrate()
 
