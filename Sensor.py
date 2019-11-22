e21f8ec5c36d2695600ebc7cb566b0970f30a4c3
Brett <fotheringhambrett@gmail.com>
2019-11-22 10:24:52 -0700

    For Chris!

--- Sensor.py
+++ Sensor.py
@@ -1,23 +1,23 @@
 import time
-import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+#import board
+#import busio
+#import adafruit_ads1x15.ads1115 as ADS
+#from adafruit_ads1x15.analog_in import AnalogIn
 import numpy as np
 
 class PressureSensor:
     def __init__(self):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        ads = ADS.ADS1115(i2c, address=0x48)
-        self.chan = AnalogIn(ads, ADS.P0)
+        # i2c = busio.I2C(board.SCL, board.SDA)
+        # ads = ADS.ADS1115(i2c, address=0x48)
+        # self.chan = AnalogIn(ads, ADS.P0)
         #For the tuple, (x,y), x is the voltage at the pressure and y is the pressure
         self.range = 4.5
         self.high_point = [0,0]
         self.low_point = [0,0]
         self.slope = 0
         self.y_intercept = 0
-        self.cal_file = '/home/pi/pressure_visualization/calibration.txt'
-        # self.cal_file = '/home/brett/pressure_visualizer/calibration.txt'
+        # self.cal_file = '/home/pi/pressure_visualization/calibration.txt'
+        self.cal_file = '/home/brett/pressure_visualizer/calibration.txt'
 
         file = open(self.cal_file, 'r')
         for line in file:
@@ -28,20 +28,20 @@ class PressureSensor:
         print(self.slope,self.y_intercept)
         file.close()
     def set_high_point(self,pressure):
-        #voltage = np.random.randint(self.range)
-        voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
+        voltage = np.random.randint(self.range)
+        #voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
         self.high_point = [voltage,pressure]
         self.calibrate()
 
     def set_low_point(self,pressure):
-        voltage = self.chan.voltage
-        #voltage = np.random.randint(self.range)
+        #voltage = self.chan.voltage
+        voltage = np.random.randint(self.range)
         self.low_point = [voltage,pressure]
         self.calibrate()
 
     def get_pressure(self):
-        # voltage = np.random.randint(self.range)
-        voltage = self.chan.voltage
+        voltage = np.random.randint(self.range)
+        #voltage = self.chan.voltage
         return self.slope*voltage + self.y_intercept
 
     def calibrate(self):
@@ -56,20 +56,20 @@ class PressureSensor:
 
 class TemperatureSensor:
     def __init__(self,AnalogChannel=0):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        ads = ADS.ADS1115(i2c, address=0x49)
-        self.chan = AnalogIn(ads, ADS.P0)
+        #i2c = busio.I2C(board.SCL, board.SDA)
+        #ads = ADS.ADS1115(i2c, address=0x49)
+        #self.chan = AnalogIn(ads, ADS.P0)
         #For the tuple, (x,y), x is the voltage at the pressure and y is the pressure
-        if AnalogChannel == 0:
-                self.chan = AnalogIn(ads, ADS.P0)
-        if AnalogChannel == 1:
-                self.chan = AnalogIn(ads, ADS.P1)
-   
-        if AnalogChannel == 2:
-                self.chan = AnalogIn(ads, ADS.P2)
-        if AnalogChannel == 3:
-                self.chan = AnalogIn(ads, ADS.P3)
-        self.channel = AnalogChannel
+        # if AnalogChannel == 0:
+        #         self.chan = AnalogIn(ads, ADS.P0)
+        # if AnalogChannel == 1:
+        #         self.chan = AnalogIn(ads, ADS.P1)
+        #
+        # if AnalogChannel == 2:
+        #         self.chan = AnalogIn(ads, ADS.P2)
+        # if AnalogChannel == 3:
+        #         self.chan = AnalogIn(ads, ADS.P3)
+        # self.channel = AnalogChannel
         self.slope_idx = AnalogChannel*2 #This comes from how we how the temp_calibration.txt file is written and read from
         self.inter_idx = AnalogChannel*2 + 1
         self.range = 4.5
@@ -77,8 +77,8 @@ class TemperatureSensor:
         self.low_point = [0,0]
         self.slope = 0
         self.y_intercept = 0
-        self.cal_file = '/home/pi/pressure_visualization/temperature_calibration.txt'
-        # self.cal_file = '/home/brett/pressure_visualizer/temperature_calibration.txt'
+        self.cal_file = '/home/brett/pressure_visualizer/temperature_calibration.txt'
+        # self.cal_file = '/home/pi/pressure_visualization/temperature_calibration.txt'
 
         file = open(self.cal_file, 'r')
         for idx, line in enumerate(file):
@@ -90,20 +90,20 @@ class TemperatureSensor:
         file.close()
 
     def set_high_point(self,temperature):
-        # voltage = np.random.randint(self.range)
-        voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
+        voltage = np.random.randint(self.range)
+        # voltage = self.chan.voltage #TODO: Read in and average multiple values to eliminate noise
         self.high_point = [voltage,temperature]
         self.calibrate()
 
     def set_low_point(self,temperature):
-        voltage = self.chan.voltage
-        # voltage = np.random.randint(self.range)
+        # voltage = self.chan.voltage
+        voltage = np.random.randint(self.range)
         self.low_point = [voltage,temperature]
         self.calibrate()
 
     def get_temperature(self):
-        # voltage = np.random.randint(self.range)
-        voltage = self.chan.voltage
+        voltage = np.random.randint(self.range)
+        # voltage = self.chan.voltage
         return self.slope*voltage + self.y_intercept
 
     def calibrate(self):
